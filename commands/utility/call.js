542bcc17ad4d69139652aa8bbb2267c452a78d21
SilverCDCCD <kivante@live.com>
2025-12-04 08:06:24 -0500

    Add initial files for testing Slambot connection.

---
+++ commands/utility/call.js
@@ -0,0 +1,9 @@
+const { SlashCommandBuilder } = require("discord.js");
+module.exports = {
+    "data": new SlashCommandBuilder()
+        .setName("call")
+        .setDescription("Call Slambot"),
+    async execute(interaction) {
+        await interaction.reply("Slambot, at the ready!");
+    }
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags, Message } = require("discord.js");
+const { discordToken } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+
+require("dotenv").config();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log("Slambot, online!");
+});
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands")
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file)
+        const command = require(filePath);
+        if (!('data' in command))
+            console.log(`[WARNING] The command at ${filePath} is missing required "data" property.`);
+        else if (!('execute' in command))
+            console.log(`[WARNING] The command at ${filePath} is missing required "execute" property.`);
+		else
+			client.commands.set(command.data.name, command);
+    }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand())
+        return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error( `Error! Command not found: ${interaction.commandName}`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    }
+    catch (ex) {
+        console.error(`Error: ${error}`);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: `Error! Counot execute command ${interaction.commandName}`,
+                flags: MessageFlags.Ephemeral
+            });
+        }
+        else {
+            await interaction.reply({
+                content: `Error! Counot execute command ${interaction.commandName}`,
+                flags: MessageFlags.Ephemeral
+            });
+        }
+    }
+});
+
+client.login(discordToken);
